0ef8e874dc48c8ca029086dce03587f57bff5c94
campbells <tranbaolam26902@gmail.com>
2023-08-21 15:18:59 +0700

    apply facade pattern to icons and images

---
+++ src/junie-store-ui/src/assets/icons/index.jsx
@@ -0,0 +1,13 @@
+import cartIcon from './cart.svg';
+import closeIcon from './close.svg';
+import hamburgerIcon from './hamburger.svg';
+import searchIcon from './search.svg';
+import userIcon from './user.svg';
+
+export const icons = {
+    cart: cartIcon,
+    close: closeIcon,
+    hamburger: hamburgerIcon,
+    search: searchIcon,
+    user: userIcon
+};
---
+++ src/junie-store-ui/src/assets/images/index.jsx
@@ -0,0 +1,9 @@
+import logoImage from './logo.png';
+import homeHeroBannerDefaultImage from './home-hero-banner-default.jpg';
+import homeHeroBannerMobileImage from './home-hero-banner-mobile.jpg';
+
+export const images = {
+    logo: logoImage,
+    homeHeroBannerDefault: homeHeroBannerDefaultImage,
+    homeHeroBannerMobile: homeHeroBannerMobileImage
+};
