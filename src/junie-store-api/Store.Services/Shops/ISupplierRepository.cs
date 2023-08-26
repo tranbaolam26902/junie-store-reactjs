c03d476959c3ad1ecb69f531521421f70210bb88
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-08-27 00:10:38 +0700

    Add supplier repo

---
+++ src/junie-store-api/Store.Services/Shops/ISupplierRepository.cs
@@ -0,0 +1,19 @@
+﻿using Store.Core.Contracts;
+using Store.Core.Entities;
+
+namespace Store.Services.Shops;
+
+public interface ISupplierRepository
+{
+	Task<IPagedList<T>> GetPagedSuppliersAsync<T>(
+		string keyword,
+		IPagingParams pagingParams,
+		Func<IQueryable<Supplier>, IQueryable<T>> mapper);
+
+	Task<Supplier> GetSupplierByIdAsync(Guid supplierId, CancellationToken cancellationToken = default);
+
+	Task AddOrUpdateSupplierAsync(
+		Supplier supplier,
+		CancellationToken cancellation = default);
+	
+}
