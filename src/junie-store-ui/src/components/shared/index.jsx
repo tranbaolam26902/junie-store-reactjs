9a3f1f36a5ef85cd19398ac2bb5b1a7b8e43d406
campbells <tranbaolam26902@gmail.com>
2023-09-06 23:58:26 +0700

    add input component

--- src/junie-store-ui/src/components/shared/index.jsx
+++ src/junie-store-ui/src/components/shared/index.jsx
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
 export { default as Container } from './Container';
+export { default as Input } from './Input';
 export { default as Pager } from './Pager';
 export { default as SidebarModal } from './SidebarModal';
