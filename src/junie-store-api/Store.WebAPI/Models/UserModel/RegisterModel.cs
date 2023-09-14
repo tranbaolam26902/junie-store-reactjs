86a891b6f8f15f054b307fe44c9840fe00089f68
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-15 01:40:49 +0700

    update register model

--- src/junie-store-api/Store.WebAPI/Models/UserModel/RegisterModel.cs
+++ src/junie-store-api/Store.WebAPI/Models/UserModel/RegisterModel.cs
@@ -2,17 +2,11 @@
 
 public class RegisterModel
 {
-	public string Name { get; set; }
-
 	public string Email { get; set; }
-
-	public string Password { get; set; }
 
 	public string Username { get; set; }
 
-	public string Phone { get; set; }
-
-	public string Address { get; set; }
+	public string Password { get; set; }
 }
 
 public class UserRolesEditModel
