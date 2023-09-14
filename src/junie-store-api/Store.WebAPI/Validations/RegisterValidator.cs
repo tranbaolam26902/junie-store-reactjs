570bd37e3848723f35faecda6a954eda5bbc3b03
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-15 01:41:19 +0700

    update validator

--- src/junie-store-api/Store.WebAPI/Validations/RegisterValidator.cs
+++ src/junie-store-api/Store.WebAPI/Validations/RegisterValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Store.WebAPI.Models.UserModel;
+using System.Text.RegularExpressions;
 
 namespace Store.WebAPI.Validations;
 
@@ -14,19 +15,24 @@ public class RegisterValidator : AbstractValidator<RegisterModel>
 			.NotEmpty()
 			.WithMessage("Email không được để trống.");
 
-		RuleFor(s => s.Name)
+		RuleFor(s => s.Username)
 			.NotEmpty()
-			.WithMessage("Tên không được để trống.")
+			.WithMessage("Tên đăng nhập không được để trống.")
 			.MaximumLength(128)
-			.WithMessage("Tên không được nhiều hơn 128 ký tự.");
-
-		RuleFor(s => s.Phone)
-			.Matches(@"(84|0[3|5|7|8|9])+([0-9]{8})\b")
-			.When(s => !string.IsNullOrWhiteSpace(s.Phone))
-			.WithMessage("Không đúng định dạng số điện thoại.");
+			.WithMessage("Tên đăng nhập không được nhiều hơn 128 ký tự.")
+			.Must(BeAValidUsername).WithMessage("Tên đăng nhập không hợp lệ."); ;
 
 		RuleFor(s => s.Password)
 			.NotEmpty().WithMessage("Mật khẩu không được để trống.")
 			.MinimumLength(8).WithMessage("Mật khẩu phải có ít nhất 8 ký tự.");
+	}
+
+	private bool BeAValidUsername(string username)
+	{
+		// Define the regular expression pattern for a valid username (no spaces)
+		string pattern = @"^\S+$";
+
+		// Use Regex.IsMatch to check if the username matches the pattern
+		return Regex.IsMatch(username, pattern);
 	}
 }
