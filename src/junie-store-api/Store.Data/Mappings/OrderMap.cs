5e09e1e14f5e0f5f1a92701a78f600175dd84aac
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-16 09:43:38 +0700

    remove fk user order

--- src/junie-store-api/Store.Data/Mappings/OrderMap.cs
+++ src/junie-store-api/Store.Data/Mappings/OrderMap.cs
@@ -11,20 +11,17 @@ public class OrderMap : IEntityTypeConfiguration<Order>
 		// Configure the primary key
 		builder.HasKey(o => o.Id);
 		
-		builder.Property(o => o.FirstName)
+		builder.Property(o => o.Name)
 			.IsRequired()
-			.HasMaxLength(56);
+			.HasMaxLength(128);
 		
-		builder.Property(o => o.LastName)
-			.HasMaxLength(56);
-
 		builder.Property(s => s.CodeOrder)
 			.IsRequired()
 			.HasMaxLength(128);
 
 		builder.Property(o => o.Email)
 			.IsRequired()
-			.HasMaxLength(100);
+			.HasMaxLength(128);
 
 		builder.Property(o => o.ShipAddress)
 			.IsRequired()
@@ -35,7 +32,7 @@ public class OrderMap : IEntityTypeConfiguration<Order>
 			.HasMaxLength(12);
 
 		builder.Property(o => o.Note)
-			.HasMaxLength(512);
+			.HasMaxLength(1024);
 
 		// Configure the details collection
 		builder.HasMany(o => o.Details)
@@ -43,12 +40,6 @@ public class OrderMap : IEntityTypeConfiguration<Order>
 			.HasForeignKey(d => d.OrderId)
 			.HasConstraintName("FK_Orders_Details")
 			.OnDelete(DeleteBehavior.Cascade);
-
-		builder.HasOne(o => o.User)
-			.WithMany(u => u.Orders)
-			.HasForeignKey(o => o.UserId)
-			.HasConstraintName("FK_Orders_Users")
-			.IsRequired(false);
 
 		// Configure the timestamps
 		builder.Property(o => o.OrderDate)
