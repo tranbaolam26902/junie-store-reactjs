25d3191ece37376c5cfdbe627694abb1a048da8c
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-17 02:05:42 +0700

    update product filter

--- src/junie-store-api/Store.WebAPI/Models/ProductModel/ProductFilterModel.cs
+++ src/junie-store-api/Store.WebAPI/Models/ProductModel/ProductFilterModel.cs
@@ -4,5 +4,17 @@ public class ProductFilterModel : PagingModel
 {
 	public string Keyword { get; set; }
 	public string CategorySlug { get; set; }
+
 	public string ProductSlug { get; set; }
+
+
+	public bool? Active { get; set; }
+
+	public bool? IsDeleted { get; set; }
+
+	public int? Year { get; set; }
+
+	public int? Month { get; set; }
+
+	public int? Day { get; set; }
 }
