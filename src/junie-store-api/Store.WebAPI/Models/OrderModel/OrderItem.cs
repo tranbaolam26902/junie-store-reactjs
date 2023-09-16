4f5cfe41342e391c6df4feede8d4731b234ecbf6
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-17 02:05:27 +0700

    add order item

---
+++ src/junie-store-api/Store.WebAPI/Models/OrderModel/OrderItem.cs
@@ -0,0 +1,47 @@
+﻿using Store.Core.Entities;
+
+namespace Store.WebAPI.Models.OrderModel;
+
+public class OrderItem
+{
+	public Guid Id { get; set; }
+
+	public string CodeOrder { get; set; }
+
+	public DateTime OrderDate { get; set; }
+
+	public OrderStatus Status { get; set; }
+
+	public string Name { get; set; }
+
+	public string Email { get; set; }
+
+	public string ShipAddress { get; set; }
+
+	public string Phone { get; set; } // Số điện thoại người nhận
+
+	public string Note { get; set; }
+
+	public double Total => CalculateTotal();
+
+	private double CalculateTotal()
+	{
+		var total = Details.Sum(s => s.Price);
+		if (Discount != null)
+		{
+			total *= Discount.DiscountPercentage / 100;
+		}
+
+		return total;
+	}
+
+	public DiscountDto Discount { get; set; }
+	public IList<OrderDetailItem> Details { get; set; }
+}
+
+public class OrderDetailItem : OrderDetailDto
+{
+	public string Name { get; set; }
+
+	public string Sku { get; set; }
+}
