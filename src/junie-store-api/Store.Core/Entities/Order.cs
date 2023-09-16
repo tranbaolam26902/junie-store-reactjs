ba1a7872d807ba10abdbbaefafed3d8b0f098c71
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-16 09:43:04 +0700

    update order

--- src/junie-store-api/Store.Core/Entities/Order.cs
+++ src/junie-store-api/Store.Core/Entities/Order.cs
@@ -25,9 +25,7 @@ public class Order : IEntity
 
 	public OrderStatus Status { get; set; }
 
-	public string FirstName { get; set; }
-
-	public string LastName { get; set; }
+	public string Name { get; set; }
 
 	public string Email { get; set; }
 
