b8b4edaac56b754745c937833220abe8c3da655d
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-18 15:29:39 +0700

    add category item

---
+++ src/junie-store-api/Store.Core/DTO/CategoryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Core.DTO;
+
+public class CategoryItem
+{
+	public Guid Id { get; set; }
+
+	public string Name { get; set; }
+
+	public string UrlSlug { get; set; }
+
+	public string Description { get; set; }
+
+	public bool ShowOnMenu { get; set; }
+
+	public bool IsDeleted { get; set; }
+
+	public int ProductCount { get; set; }
+}
