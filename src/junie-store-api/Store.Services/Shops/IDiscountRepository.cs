3c5f79f30504e0df8631cfb49f57c826c3c2bbe6
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-19 20:24:10 +0700

    add discount repo

---
+++ src/junie-store-api/Store.Services/Shops/IDiscountRepository.cs
@@ -0,0 +1,22 @@
+﻿using Store.Core.Contracts;
+using Store.Core.Entities;
+using Store.Core.Queries;
+
+namespace Store.Services.Shops;
+
+public interface IDiscountRepository
+{
+	Task<IPagedList<T>> GetPagedDiscountsAsync<T>(
+		IDiscountQuery condition,
+		IPagingParams pagingParams,
+		Func<IQueryable<Discount>,  IQueryable<T>> mapper);
+
+	Task<Discount> AddOrUpdateDiscountAsync(Discount discount, CancellationToken cancellation = default);
+
+
+	Task<bool> ToggleShowOnMenuAsync(Guid discountId, CancellationToken cancellation = default);
+
+	Task<bool> ToggleActiveAsync(Guid discountId, CancellationToken cancellation = default);
+
+	Task<bool> DeleteDiscountAsync(Guid discountId, CancellationToken cancellation = default);
+}
