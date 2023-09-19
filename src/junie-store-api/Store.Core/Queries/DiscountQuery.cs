1057cf83660f965f6ed7e37529fcbce03e3cd438
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-19 20:23:54 +0700

    add discount query

---
+++ src/junie-store-api/Store.Core/Queries/DiscountQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Core.Queries;
+
+public class DiscountQuery : IDiscountQuery
+{
+	public string Code { get; set; } = "";
+	public int Quantity { get; set; } = 0;
+	public float MinPrice { get; set; } = 0f;
+	public float DiscountPercent { get; set; } = 0f;
+	public int? Year { get; set; } = 0;
+	public int? Month { get; set; } = 0;
+	public int? Day { get; set; } = 0;
+	public bool ShowOnMenu { get; set; } = false;
+}
---
+++ src/junie-store-api/Store.Core/Queries/IDiscountQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace Store.Core.Queries;
+
+public interface IDiscountQuery
+{
+	public string Code { get; set; }
+
+	public int Quantity { get; set; }
+
+	public float MinPrice { get; set; }
+
+	public float DiscountPercent { get; set; }
+
+	public int? Year { get; set; }
+
+	public int? Month { get; set; }
+
+	public int? Day { get; set; }
+
+	public bool ShowOnMenu { get; set; }
+}
