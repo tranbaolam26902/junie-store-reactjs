0f622c11c493fc107176ec1c7d11842077675e5f
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-20 16:51:33 +0700

    Add migration discount amount

---
+++ src/junie-store-api/Store.Data/Migrations/20230920082843_UpdateDiscountAmount.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDiscountAmount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DiscountPercentage",
+                table: "Orders",
+                newName: "DiscountAmount");
+
+            migrationBuilder.RenameColumn(
+                name: "ShowOnMenu",
+                table: "Discounts",
+                newName: "IsDiscountPercentage");
+
+            migrationBuilder.RenameColumn(
+                name: "DiscountPercentage",
+                table: "Discounts",
+                newName: "DiscountAmount");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDiscountPercentage",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDiscountPercentage",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "DiscountAmount",
+                table: "Orders",
+                newName: "DiscountPercentage");
+
+            migrationBuilder.RenameColumn(
+                name: "IsDiscountPercentage",
+                table: "Discounts",
+                newName: "ShowOnMenu");
+
+            migrationBuilder.RenameColumn(
+                name: "DiscountAmount",
+                table: "Discounts",
+                newName: "DiscountPercentage");
+        }
+    }
+}
