bcb440b12e44817bd84aa27e56f1711ddee6198a
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-20 18:45:43 +0700

    add discount edit model

---
+++ src/junie-store-api/Store.WebAPI/Models/DiscountModel/DiscountEditModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Store.WebAPI.Models.DiscountModel;
+
+public class DiscountEditModel
+{
+	public int Quantity { get; set; }
+
+	public float MinPrice { get; set; }
+
+	public DateTime ExpiryDate { get; set; }
+
+	public string Code { get; set; }
+
+	public float DiscountAmount { get; set; }
+
+	public bool IsDiscountPercentage { get; set; }
+}
