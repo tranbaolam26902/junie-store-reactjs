eee7af19797e7aafb39a0f38eee8581ff2b1bf34
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-25 15:20:40 +0700

    add password validator

---
+++ src/junie-store-api/Store.WebAPI/Validations/PasswordValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Store.WebAPI.Models.ProductModel;
+using Store.WebAPI.Models.UserModel;
+
+namespace Store.WebAPI.Validations;
+
+public class PasswordValidator : AbstractValidator<PasswordEditModel>
+{
+	public PasswordValidator()
+	{
+		RuleFor(s => s.ConfirmPassword)
+			.NotEmpty().WithMessage("Mật khẩu không được để trống");
+
+		RuleFor(s => s.NewPassword)
+			.NotEmpty().WithMessage("Mật khẩu mới không được để trống");
+
+		RuleFor(s => s.ConfirmPassword)
+			.NotEmpty().WithMessage("Xác nhận mật khẩu không được để trống");
+	}
+}
