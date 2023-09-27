297f2a2a0c6add0a23423a334899d330e43bb166
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-09-27 14:49:11 +0700

    add category query

--- src/junie-store-api/Store.Core/Queries/CategoryQuery.cs
+++ src/junie-store-api/Store.Core/Queries/CategoryQuery.cs
@@ -3,5 +3,10 @@
 public class CategoryQuery : ICategoryQuery
 {
 	public string Keyword { get; set; } = "";
+
 	public string UrlSlug { get; set; } = "";
+
+	public bool ShowOnMenu { get; set; } = false;
+	
+	public bool IsDeleted { get; set; } = false;
 }
--- src/junie-store-api/Store.Core/Queries/ICategoryQuery.cs
+++ src/junie-store-api/Store.Core/Queries/ICategoryQuery.cs
@@ -3,6 +3,10 @@
 public interface ICategoryQuery
 {
 	public string Keyword { get; set; }
-	
+
 	public string UrlSlug { get; set; }
+
+	public bool ShowOnMenu { get; set; }
+
+	public bool IsDeleted { get; set; }
 }
